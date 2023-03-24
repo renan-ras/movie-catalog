987163d0e3af7c064281f34c3b88909de7d6094e
Renan Antonio da Silva <renanras@gmail.com>
2023-03-24 14:30:15 -0300

    index ok; Seleção se mantém

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @movies = Movie.published
+    # @movies = Movie.published
+    # @radio_selection = [true, false, false]
 
     if params[:filter] == 'released'
       @movies = Movie.published.where("release_year <= ?", Time.now.year)
+      @radio_selection = [false, true, false]
     elsif params[:filter] == 'not_released'
       @movies = Movie.published.where("release_year > ?", Time.now.year)
+      @radio_selection = [false, false, true]
     else
       @movies = Movie.published
+      @radio_selection = [true, false, false]
     end
 
   end
